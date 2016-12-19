20fe49398674936f12a9f201bba1112ef8c19211
wtffly <wtffly@163.com>
2016-12-19 11:07:34 +0800

    源码及Sample

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # DroidBinding_StepsView
 anton46/Android-StepsView 组件的Xamarin.Android的绑定。
+
+# Android-StepsView
+
+![Example image](./image1.png) ![Example image](./image2.png)
+
+## Usage
+
+```xml
+ <com.anton46.stepsview.StepsView
+        xmlns:app="http://schemas.android.com/apk/res-auto"
+        android:id="@+id/stepsView"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        />
+```
+
+```C#
+stepsView.SetLabels(steps)
+         .SetBarColorIndicator(ContextCompat.GetColor(this, Resource.Color.material_blue_grey_800))
+         .SetProgressColorIndicator(ContextCompat.GetColor(this, Resource.Color.orange))
+         .SetLabelColorIndicator(ContextCompat.GetColor(this, Resource.Color.orange))
+         .SetCompletedPosition(0)
+         .DrawView();
+```
+
+Android-StepsView Developed by
+========================
+* Anton Nurdin Tuhadiansyah (anton.work19@gmail.com)
+* [anton46.com][1]
+
+[1]: http://anton46.com
+
+License
+-----------
+
+```
+Copyright 2015 Anton Nurdin Tuhadiansyah
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ Sample/MainActivity.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Linq;
+using DroidStepsView;
+using Android.Support.V4.Content;
+
+namespace Binding_StepsView_Sample
+{
+    [Activity(
+        Label = "@string/app_name", 
+        MainLauncher = true, 
+        Icon = "@mipmap/ic_launcher",
+        Theme = "@style/AppTheme",
+        ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    public class MainActivity : Activity
+    {
+        private string[] views = {"View 1", "View 2", "View 3", "View 4", "View 5", "View 6",
+            "View 7", "View 8", "View 9", "View 10", "View 11", "View 12"};
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            ListView mListView = FindViewById<ListView>(Resource.Id.list);
+            
+            MyAdapter adapter = new MyAdapter(this, 0);
+            adapter.AddAll(views.ToList());
+            mListView.Adapter = adapter;
+        }
+
+        private class MyAdapter : ArrayAdapter<string>
+        {
+            string[] labels = { "Step 1", "Step 2", "Step 3", "Step 4", "Step 5" };
+            int[] colors = { Resource.Color.red, Resource.Color.blue, Resource.Color.orange };
+            Activity context;
+            public MyAdapter(Activity context, int resource)
+                : base(context, resource)
+            {
+                this.context = context;
+            }
+
+            public override View GetView(int position, View convertView, ViewGroup parent)
+            {
+                View view = convertView; // re-use an existing view, if one is available
+                if (view == null) // otherwise create a new one
+                    view = context.LayoutInflater.Inflate(Resource.Layout.row, null);
+
+                view.FindViewById<TextView>(Resource.Id.label).Text = GetItem(position);
+                var color = colors[position % colors.Length];
+                view.FindViewById<StepsView>(Resource.Id.stepsView)
+                    .SetCompletedPosition(position % labels.Length)
+                    .SetLabels(labels)
+                    .SetBarColorIndicator(ContextCompat.GetColor(context, Resource.Color.material_blue_grey_800))
+                    .SetProgressColorIndicator(ContextCompat.GetColor(context, color))
+                    .SetLabelColorIndicator(ContextCompat.GetColor(context, color))
+                    .DrawView();
+
+                return view;
+            }
+        }
+    }
+}
+
